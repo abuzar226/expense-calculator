94b56fb167bc06c5a0563278e054b1434cbee754
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2022-04-13 15:43:14 +0530

    date format corrected

--- src/component/ExpenseDate.js
+++ src/component/ExpenseDate.js
@@ -1,17 +1,20 @@
+import React from 'react'
 import './ExpenseDate.css'
 
+
 function ExpenseDate(props){
-    const month = props.date.toLocaleString('en-US',{months: 'long'});
-    const day= props.date.toLocaleString('en-US', { day:' 2-digit' });
-    const year = props.year.getFullYear();
+    const month = props.date.toLocaleString('en-US',{month: 'long'});
+    const day= props.date.toLocaleString("en-US",{ day: 'numeric'});
+    const year = props.date.getFullYear({year: 'numeric'});
 
 
-    return(
+    return(<>
     <div className="expense-date">
     <div>{month} </div>
     <div>{year}</div>
     <div>{day}</div>
     </div>
+    </>
     );
 }
 
